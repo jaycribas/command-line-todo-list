25b2b032e84054f7dda5879961bcd793dcb036ea
Jay Cribas <feverjay@gmail.com>
2017-07-11 17:37:30 -0700

    Lists todos in a console table

--- commands/list.js
+++ commands/list.js
@@ -1,1 +1,24 @@
 // list - lists the incomplete tasks -- fs.readFileSync
+const fs = require('fs')
+const json = require('../tasks.json')
+require('console.table')
+
+const listTodos = function(){
+  let todoList = []
+
+  for(let i = 0; i < json.todos.length; i++){
+    let task = {
+      "ID": json.todos[i].id,
+      "Description": json.todos[i].description
+    }
+    todoList.push(task)
+  }
+
+  console.table('To Do List', todoList)
+  if(todoList.length == 1 ){
+    return console.log(`${todoList.length} task.`)
+  }
+  console.log(`${todoList.length} tasks.`)
+}
+
+module.exports = { listTodos }
